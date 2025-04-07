ce665b6ac76a59427c34a827a7cc26426e833eb8
avashokkumar <30625101+avashokkumar@users.noreply.github.com>
2025-04-07 16:45:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express ();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 console.log('Port from env file',process.env.PORT)
-app.listen(PORT, 0.0.0.0, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log("Server Listening on PORT:", PORT);
 });
 
